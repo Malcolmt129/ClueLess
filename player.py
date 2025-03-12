4e854f7d05ef0895a320a115f0f8c42c515bb762
mariahito <mh4wt@virginia.edu>
2025-03-12 13:30:39 -0400

    changes to UI code

--- player.py
+++ player.py
@@ -1,16 +1,12 @@
-import characters
-
-
 class Player:
-
     def __init__(self, name):
         self.name = name
-        self.cards = []
-        self.location = None
-    
-    def receive_card(self, card):
-        self.cards.append(card)
+        self.position = (0, 0)  # Default starting position
 
-    def __repr__(self):
-        return f"{self.name} (Card: {self.cards})"
+    def set_position(self, row, col):
+        """Sets the player's position on the board."""
+        self.position = (row, col)
 
+    def get_position(self):
+        """Returns the player's current position."""
+        return self.position
