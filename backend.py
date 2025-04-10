74e70deb6c93554c24d0a0143f08bad12b36fd8c
Tyler Protivnak <14126816+tjpro@users.noreply.github.com>
2025-04-09 20:08:07 -0400

    Adding sleep to fix timing issue...

--- backend.py
+++ backend.py
@@ -10,6 +10,7 @@ import game_logic
 import traceback
 import struct
 import sys
+import time
 
 # Create a module-specific logger
 logger = logging.getLogger(__name__)
@@ -37,67 +38,71 @@ def start_server():
     try:
         while not game.is_over:
             # Monitor sockets for readability
-            read_sockets, _, _ = select.select(sockets_list, [], [])
-            for sock in read_sockets:
-                if sock is server_socket:
-                    # Handle new connections
-                    client_socket, client_address = server_socket.accept()
-                    logger.info(f"New connection from {client_address}")
-                    client_socket.setblocking(False)
-                    client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER, linger_struct)
-                    sockets_list.append(client_socket)
-                    client_socket.sendall(game.get_welcome_message(client_address[1]).to_json_str().encode())
-                    # client_socket.sendall(game.get_state_message().to_json_str().encode())
-                    clients[client_address[1]] = client_socket
-                else:
-                    # Handle client messages
-                    try:
-                        data = sock.recv(1024)
-                        if data:
-                            decoded_data = data.decode()
-                            logger.debug(f"Received from {sock.getpeername()}: {decoded_data}")
-                            # Process the received JSON message using message objects
-                            try:
-                                msg_data = json.loads(decoded_data)
-                                message = message_from_json(msg_data)
-                                logger.debug(f"Message: {message}")
-                                if message:
-                                    outgoing_queue = game.process_message(message)
-                                    logger.debug(f"Outgoing messages: {outgoing_queue}")
-                                    for m in outgoing_queue:
-                                        logger.debug(f"Sending message: {m}")
-                                        logger.debug(f"Sending to: {clients[m[1]]}")
-                                        clients[m[1]].sendall(m[0].to_json_str().encode())
-                                    if not game.state.game_started and message.type == 'join' and len(game.players) == MAX_CLIENTS:
-                                        logger.debug("Starting game")
-                                        outgoing_queue = game.start_game()
+            read_sockets, _, _ = select.select(sockets_list, [], [], 1.0)
+            if read_sockets:
+                for sock in read_sockets:
+                    if sock is server_socket:
+                        # Handle new connections
+                        client_socket, client_address = server_socket.accept()
+                        logger.info(f"New connection from {client_address}")
+                        client_socket.setblocking(False)
+                        client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER, linger_struct)
+                        sockets_list.append(client_socket)
+                        client_socket.sendall(game.get_welcome_message(client_address[1]).to_json_str().encode())
+                        # client_socket.sendall(game.get_state_message().to_json_str().encode())
+                        clients[client_address[1]] = client_socket
+                    else:
+                        # Handle client messages
+                        try:
+                            data = sock.recv(1024)
+                            if data:
+                                decoded_data = data.decode()
+                                logger.debug(f"Received from {sock.getpeername()}: {decoded_data}")
+                                # Process the received JSON message using message objects
+                                try:
+                                    msg_data = json.loads(decoded_data)
+                                    message = message_from_json(msg_data)
+                                    logger.debug(f"Message: {message}")
+                                    if message:
+                                        outgoing_queue = game.process_message(message)
                                         logger.debug(f"Outgoing messages: {outgoing_queue}")
                                         for m in outgoing_queue:
                                             logger.debug(f"Sending message: {m}")
                                             logger.debug(f"Sending to: {clients[m[1]]}")
                                             clients[m[1]].sendall(m[0].to_json_str().encode())
-                                else:
+                                        if not game.state.game_started and message.type == 'join' and len(game.players) == MAX_CLIENTS:
+                                            time.sleep(1)
+                                            logger.debug("Starting game")
+                                            outgoing_queue = game.start_game()
+                                            logger.debug(f"Outgoing messages: {outgoing_queue}")
+                                            for m in outgoing_queue:
+                                                logger.debug(f"Sending message: {m}")
+                                                logger.debug(f"Sending to: {clients[m[1]]}")
+                                                clients[m[1]].sendall(m[0].to_json_str().encode())
+                                    else:
+                                        error_response = ErrorMessage(
+                                            user_id=0,
+                                            reason="Invalid message type or data"
+                                        )
+                                        sock.sendall(error_response.to_json_str().encode())
+                                except json.JSONDecodeError:
                                     error_response = ErrorMessage(
                                         user_id=0,
-                                        reason="Invalid message type or data"
+                                        reason="Invalid JSON format"
                                     )
                                     sock.sendall(error_response.to_json_str().encode())
-                            except json.JSONDecodeError:
-                                error_response = ErrorMessage(
-                                    user_id=0,
-                                    reason="Invalid JSON format"
-                                )
-                                sock.sendall(error_response.to_json_str().encode())
-                        else:
-                            # Client disconnected
-                            logger.info(f"Client {sock.getpeername()} disconnected")
+                            else:
+                                # Client disconnected
+                                logger.info(f"Client {sock.getpeername()} disconnected")
+                                sockets_list.remove(sock)
+                                sock.close()
+                        except Exception as e:
+                            logger.error(f"Error with client {sock.getpeername()}: {e}")
+                            logger.debug(traceback.format_exc())
                             sockets_list.remove(sock)
                             sock.close()
-                    except Exception as e:
-                        logger.error(f"Error with client {sock.getpeername()}: {e}")
-                        logger.debug(traceback.format_exc())
-                        sockets_list.remove(sock)
-                        sock.close()
+            # else:
+
     except KeyboardInterrupt:
         logger.info("Server shutting down...")
     finally:
